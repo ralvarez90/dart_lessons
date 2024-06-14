d91c1b9ebb4e51a988194720c22870ac9a425dfa
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-06-14 17:10:10 -0600

    Imprime mensaje

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,9 @@
-import 'package:dart_lessons/lessons.dart' as dart_lessons;
+import 'package:dart_lessons/lessons.dart';
 import 'package:dart_lessons/tools.dart';
 
 void main(List<String> arguments) {
   /// Ls01. Show message with print function
-  dart_lessons.ls01HelloWorld();
+  ls01HelloWorld();
 
   // end application
   systemPause();
